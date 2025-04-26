47f357bc2755e4419dcf83287d5d379eb582f30f
kipngetich204 <153486695+kipngetich204@users.noreply.github.com>
2025-04-26 18:48:25 +0300

    first import

---
+++ my-app/src/Header.jsx
@@ -0,0 +1,19 @@
+function header(){
+    return(
+        <header>
+            <h1>my first react web</h1>
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>home</li>
+                    <li>home</li>
+                    <li>home</li>
+                    <li>home</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default header
